6b262b5629d6a3b53e8e8c07386e90c047b15937
zezerinodev <mattoszz@hotmail.com>
2023-04-12 18:05:49 -0300

    Listagem de produtos concluida

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -17,6 +17,7 @@ import { AddItemController } from '../controllers/order/AddItemController';
 import { RemoveItemController } from '../controllers/order/RemoveItemController';
 
 import { SendOrderController } from '../controllers/order/SendOrderController';
+import { ListOrdersController } from '../controllers/order/ListOrdersController';
 
 import { isAuthenticated } from '../middlewares/isAuthenticated';
 import uploadConfig from '../config/multer';
@@ -45,5 +46,6 @@ router.post('/order/add', isAuthenticated, new AddItemController().handle);
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
 
 router.put('/order/send', isAuthenticated, new SendOrderController().handle);
+router.get('/orders', isAuthenticated, new ListOrdersController().handle);
 
 export {router};
