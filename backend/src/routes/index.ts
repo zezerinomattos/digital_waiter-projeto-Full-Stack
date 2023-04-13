a4b762fa3ed3ef735946193a4b4cc882bacdc159
zezerinodev <mattoszz@hotmail.com>
2023-04-12 22:31:01 -0300

    Pedido finalizado e a API finalizada

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -21,6 +21,8 @@ import { ListOrdersController } from '../controllers/order/ListOrdersController'
 
 import { DetailOrderController } from '../controllers/order/DetailOrderController';
 
+import { FinishOrderController } from '../controllers/order/FinishOrderController';
+
 import { isAuthenticated } from '../middlewares/isAuthenticated';
 import uploadConfig from '../config/multer';
 
@@ -44,6 +46,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 // -- ROTAS ORDER
 router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
+
 router.post('/order/add', isAuthenticated, new AddItemController().handle);
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
 
@@ -51,5 +54,6 @@ router.put('/order/send', isAuthenticated, new SendOrderController().handle);
 router.get('/orders', isAuthenticated, new ListOrdersController().handle);
 
 router.get('/order/detail', isAuthenticated, new DetailOrderController().handle);
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle);
 
 export {router};
