48ceeed2bac7f7be349ff634c0a86b2f0b18da90
zezerinodev <mattoszz@hotmail.com>
2023-04-18 16:20:53 -0300

    Autenticação de login finalizada

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,7 +1,9 @@
 import { createContext, ReactNode, useState } from 'react';
 //destroyCookie é para tentarmos destruir o cookie
-import { destroyCookie } from 'nookies';
+import { destroyCookie, setCookie, parseCookies } from 'nookies';
 import Router from 'next/router';
+
+import { api } from '../services/apiClient';
 
 type AuthContextData={
     user: UserProps;
@@ -44,9 +46,35 @@ export function AuthProvider({ children }: AuthProviderProps){
     //ele converte para false se tiver alguma coisa ele fica true para controlar se está logado
 
     async function signIn({ email, password}: SignInProps){
-        //alert("Clicou")
-        console.log("Dados para logar", email)
-        console.log("Dados para logar", password)
+        try {
+            const response = await api.post('/session', {
+                email,
+                password
+            })
+            //console.log(response.data);
+            const { id, name, token } = response.data;
+
+            setCookie(undefined, '@digitalwaiter', token, {
+                maxAge: 60 * 60 * 24 * 30, //O token expira em 1 mes
+                path: "/" //Qualquer caminho era acesso a os cookies
+            })
+
+            setUser({
+                id,
+                name,
+                email,
+            })
+
+            //PASSAR PARA PROXIMAS REQUISIÇÕES O NOSSO TOKEN
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+            //REDIRECIONAR O USER PARA A PAGINA DE DASHBOARD (ULTIMOS PEDIDOS)
+            Router.push('/dashboard')
+            
+
+        } catch (err) {
+            console.log('ERRO AO ACESSAR', err);
+        }
     }
 
     return(
