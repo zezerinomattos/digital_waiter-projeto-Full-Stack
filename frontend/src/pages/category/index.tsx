267a0860aa8a0aab40e179d6e37485e9a1bc5c03
zezerinodev <mattoszz@hotmail.com>
2023-04-20 20:05:30 -0300

    Cadastro de Categoria finalizado

---
+++ frontend/src/pages/category/index.tsx
@@ -0,0 +1,60 @@
+import { useState, FormEvent } from 'react';
+import Head from "next/head";
+import { toast } from 'react-toastify';
+
+// MY IMPORTS
+import styles from './styles.module.scss';
+import { Header } from '../../components/Header';
+
+import { api } from '../../services/apiClient';
+
+export default function Category(){
+    const [name, setName] = useState('');
+
+    async function handleRegister(event: FormEvent){
+        event.preventDefault();
+
+        if(name === ''){
+            return;
+        }
+
+        await api.post('/category', {
+            name: name
+        })
+        .then(() => {
+            toast.success('CATEGORIA CADASTRADA COM SUCESSO!');
+            setName('');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    }
+
+    return(
+        <>
+            <Head>
+                <title>Nova Categoria - Digital Witer</title>
+            </Head>
+
+            <div>
+                <Header />
+
+                <main className={styles.container}>
+                    <h1>Cadastrar categorias</h1>
+
+                    <form className={styles.form} onSubmit={handleRegister}>
+                        <input type="text" 
+                        placeholder="Digite o nome da categoria" 
+                        className={styles.input} 
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        />
+
+                        <button type="submit" className={styles.buttonAdd}>Cadastrar</button>
+                    </form>
+                </main>
+            </div>
+        </>
+    );
+}
