ea3296da4299a7aedd4b318a9f1ac6e102aeb65d
zezerinodev <mattoszz@hotmail.com>
2023-05-02 17:44:36 -0300

    Requisição de categoria da pagina Order feita, o input está carregando a categoria da posição zero.

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,11 +1,13 @@
-import React, { useState, useContext} from 'react';
+import React, { useState, useContext, useEffect} from 'react';
 import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-import { useRoute, RouteProp } from '@react-navigation/native';
+import { useRoute, RouteProp, useNavigation } from '@react-navigation/native';
 
 // MY IMPORTS
 import styles from './style';
+
+import { api } from '../../service/api';
 
 type RouteDetailParams = {
     Order: {
@@ -14,23 +16,71 @@ type RouteDetailParams = {
     }
 }
 
+type CategoryProps = {
+    id: string;
+    name: string;
+}
+
 type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>;
 
 export default function Order(){
     const route = useRoute<OrderRouteProps>();
+    const navigation = useNavigation();
+
+    const [category, setCategory] = useState<CategoryProps[] | []>([]);
+    const [categorySelected, setCategorySelected] = useState<CategoryProps>();
+
+    const [amount, setAmount] = useState('1');
+
+    useEffect(() => {
+
+        loadingCategory();
+        
+    }, []);
+
+    // FUNÇÃO DE DELETAR UMA ORDER ABERTA
+    async function handleCloseOrder(){
+        try {           
+            await api.delete('/order', {
+                params: {
+                    order_id: route.params?.order_id
+                }
+            })
+            navigation.goBack();
+
+        } catch (error) {
+            console.log('ops: ' + error);
+        }
+    }
+
+    // FUNÇÃO DE LISTAGEM DE CATEGORIA
+    async function loadingCategory(){
+        await api.get('/category').then(response =>{
+            
+            setCategory(response.data);
+            setCategorySelected(response.data[0]);
+        })
+        .catch(err =>{
+            console.log('Erro:' + err);
+        });
+    }
 
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.title}>Mesa {route.params.number}</Text>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={handleCloseOrder}>
                     <Feather name='trash-2' size={25} color={'#FF3F4B'}/>
                 </TouchableOpacity>
             </View>
 
-            <TouchableOpacity style={styles.input}>
-                <Text style={{ color: '#FFF' }}>Pizzas</Text>
-            </TouchableOpacity>
+           {
+                category.length !== 0 && (
+                    <TouchableOpacity style={styles.input}>
+                        <Text style={{ color: '#FFF' }}>{categorySelected?.name}</Text>
+                    </TouchableOpacity>
+                )
+           }
 
             <TouchableOpacity style={styles.input}>
                 <Text style={{ color: '#FFF' }}>Pizza de Calabrasa</Text>
@@ -40,7 +90,7 @@ export default function Order(){
                 <Text style={styles.qtdText}>Quantidade:</Text>
                 <TextInput style={[styles.input, {width: '60%', textAlign: 'center'}]}
                     placeholderTextColor={'#F0F0F0'} keyboardType='numeric'
-                    value={'1'}
+                    value={amount} onChangeText={setAmount}
                 />
             </View>
 
