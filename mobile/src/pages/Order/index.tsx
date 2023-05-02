1b9f9454c6080934f180dff786bf4d2ca13a2dc1
zezerinodev <mattoszz@hotmail.com>
2023-05-02 16:09:50 -0300

    Tela de order criada e modelada seu estilo, já está recebendo a mesa e o order_id

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useContext} from 'react';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 import { useRoute, RouteProp } from '@react-navigation/native';
 
@@ -19,12 +20,40 @@ export default function Order(){
     const route = useRoute<OrderRouteProps>();
 
     return(
-        <View>
-            <Text>Order Aberta</Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Mesa {route.params.number}</Text>
+                <TouchableOpacity>
+                    <Feather name='trash-2' size={25} color={'#FF3F4B'}/>
+                </TouchableOpacity>
+            </View>
 
-            <Text>
-                {route.params.order_id}
-            </Text>
+            <TouchableOpacity style={styles.input}>
+                <Text style={{ color: '#FFF' }}>Pizzas</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.input}>
+                <Text style={{ color: '#FFF' }}>Pizza de Calabrasa</Text>
+            </TouchableOpacity>
+
+            <View style={styles.qtdContainer}>
+                <Text style={styles.qtdText}>Quantidade:</Text>
+                <TextInput style={[styles.input, {width: '60%', textAlign: 'center'}]}
+                    placeholderTextColor={'#F0F0F0'} keyboardType='numeric'
+                    value={'1'}
+                />
+            </View>
+
+            <View style={styles.actions}>
+                <TouchableOpacity style={styles.buttonAdd}>
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Avançar</Text>
+                </TouchableOpacity>
+            </View>
+            
         </View>
     )
 }
--- mobile/src/pages/Order/style.tsx
+++ mobile/src/pages/Order/style.tsx
@@ -1,6 +1,71 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#0D0D0D',
+        paddingVertical: '5%',
+        paddingHorizontal: '4%'
+    },
+    header: {
+        flexDirection: 'row',
+        marginBottom: 12,
+        marginTop: 24,
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#FFF',
+        marginRight: 14
+    },
+    input:{
+        backgroundColor: '#383737',
+        borderRadius: 4,
+        width: '100%',
+        height: 40,
+        marginBottom: 12,
+        justifyContent: 'center',
+        paddingHorizontal: 8,
+        color: '#FFF',
+        fontSize: 20
+    },
+    qtdContainer:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    qtdText:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#FFF'
+    },
+    actions: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between'
+    },
+    buttonAdd: {
+        width: '20%',
+        height: 40,
+        backgroundColor: '#3fd1ff',
+        borderRadius: 4,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: '#121126',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    button: {
+        width: '75%',
+        height: 40,
+        backgroundColor: '#3fffa3',
+        borderRadius: 4,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 
 });
 
